0a9988dab6f8b2a7cb7fc64e9f806fe852d03940
megutamago <naoya05280708@gmail.com>
2023-08-13 12:05:21 +0900

    test [skip ci]

--- main.tf
+++ main.tf
@@ -1,22 +1,15 @@
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region = "ap-northeast-1"
-}
+#variable "aws_access_key" {}
+#variable "aws_secret_key" {}
+#
+#provider "aws" {
+#  access_key = var.aws_access_key
+#  secret_key = var.aws_secret_key
+#  region = "ap-northeast-1"
+#}
 
 resource "aws_vpc" "test-vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
     Name = "test-vpc"
-  }
-}
-
-resource "aws_vpc" "test2-vpc" {
-  cidr_block = "10.1.0.0/16"
-  tags = {
-    Name = "test2-vpc"
   }
 }
